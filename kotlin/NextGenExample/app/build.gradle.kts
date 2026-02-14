34f36e84c58383ce50d147e424e58d680693ffae
Jason Chiu <jasoncsh@google.com>
2026-02-13 16:59:26 -0800

    Update Next-Gen examples to latest version.
    PiperOrigin-RevId: 869944023

--- kotlin/NextGenExample/app/build.gradle.kts
+++ kotlin/NextGenExample/app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
   implementation("androidx.navigation:navigation-ui-ktx:2.8.9")
   implementation("androidx.preference:preference-ktx:1.2.1")
 
-  implementation("com.google.android.libraries.ads.mobile.sdk:ads-mobile-sdk:0.23.0-beta01")
+  implementation("com.google.android.libraries.ads.mobile.sdk:ads-mobile-sdk:0.24.0-beta01")
   implementation("com.google.android.material:material:1.12.0")
 
   implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
